7618e8515e4bc6743e216b45f6254d1365affb71
Edward John Steere <edward.steere@gmail.com>
2019-11-30 06:21:01 +0200

    Add debug matrix

--- lisp/debug.lisp
+++ lisp/debug.lisp
@@ -1,6 +1,10 @@
+(ql:quickload "iterate")
+
 (defpackage :debug
   (:use :common-lisp)
-  (:export debug-hashtable))
+  (:use :iter)
+  (:export debug-hashtable)
+  (:export debug-matrix))
 
 (in-package :debug)
 
@@ -12,3 +16,16 @@
             (maphash (lambda (key value) (push (cons key value) key-values))
                      table)
             key-values)))
+
+(defun debug-matrix (matrix)
+  (destructuring-bind (x-dim y-dim) (array-dimensions matrix)
+    (format nil "
+~{~a
+~}" (iter
+      (for y from 0 below y-dim)
+      (collect
+          (apply #'concatenate
+                 'string
+                 (iter
+                   (for x from 0 below x-dim)
+                   (collect (format nil "~5d" (aref matrix x y))))))))))
