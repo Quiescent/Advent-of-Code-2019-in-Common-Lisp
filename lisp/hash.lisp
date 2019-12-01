5be52980dfdd3bcc36242b0a4bd77859e39da2ca
Edward John Steere <edward.steere@gmail.com>
2019-12-01 07:08:52 +0200

    Add hash utility

---
+++ lisp/hash.lisp
@@ -0,0 +1,13 @@
+(ql:quickload "ironclad")
+(ql:quickload "flexi-streams")
+
+(defpackage :hash
+  (:use :common-lisp)
+  (:use :ironclad)
+  (:export md5))
+
+(in-package :hash)
+
+(defun md5 (str)
+  (byte-array-to-hex-string
+   (digest-sequence :md5 (flexi-streams:string-to-octets str))))
