d521a85b4f3162fea05885afabd0e3a1139ba12f
Edward John Steere <edward.steere@gmail.com>
2019-12-22 19:13:12 +0200

    Intermediary state for tracking

--- lisp/day22.lisp
+++ lisp/day22.lisp
@@ -80,23 +80,58 @@
 
 (defun day22-part-2 (input-elements)
   "Run my solution to part two of the problem on the input in INPUT-ELEMENTS."
-  (let* ((cards (make-array (list 10007) :initial-element 0))
-         (tmp (make-array (list 10007) :initial-element 0))
-         (all-instructions (map 'vector (lambda (line) (parse-line line 10007)) input-elements))
-         (instructions-length (length all-instructions)))
-    (iter
-      (for i from 0 below 10007)
-      (setf (aref cards i) i))
+  ;; Remember the plus one for the amount of cards!!!
+  (let* ((number-of-cards 119315717514047)
+         (all-instructions (map 'vector #'parse-line-1 input-elements))
+         (instructions-length (length all-instructions))
+         (ticks 0))
+    (declare (type fixnum number-of-cards))
+    (declare (type fixnum ticks))
+    (declare (type fixnum instructions-length))
     (labels
-        ((solve (x i)
+        ((cut (number idx)
+           (let ((cut (if (< number 0)
+                          (+ number-of-cards number)
+                          number)))
+             ;; (format t "Cutting: Cut: ~a, idx: ~a, number-of-cards: ~a~%"
+             ;;         cut idx number-of-cards)
+             (declare (type fixnum idx))
+             (declare (type fixnum cut))
+             (declare (type fixnum number))
+             (mod (+ idx cut) number-of-cards)))
+         (inc (number idx)
+           (let ((idx-2 idx))
+                       ;; (format t "Incing: number-of-cards: ~a, num: ~a, mod: ~a~%"
+                       ;;         number-of-cards idx number)
+                       (declare (type fixnum idx-2))
+                       (declare (type fixnum number))
+                       (solve-modulo number-of-cards idx-2 number)))
+         (rev (idx)
+           (let ((idx-2 idx))
+             ;; (format t "Reving: rev: ~a, idx: ~a~%" idx number-of-cards)
+             (declare (type fixnum idx-2))
+             (- (1- number-of-cards) idx-2)))
+         (solve (x i)
+           (declare (type fixnum i))
            (if (eq i -1)
                x
                (let* ((idx (mod i instructions-length))
                       (looking-for (funcall (aref all-instructions idx) x)))
+                 ;;(format t "idx: ~a~%" idx)
+                 (when (eq idx 0)
+                   (incf ticks)
+                   (format t "looking-for: ~a~%" looking-for)
+                   ;(format t "ticks: ~a~%" ticks)
+                   )
                  ;; (format t "looking-for: ~a~%" x)
-                 ;; (format t "looking-for-next: ~a~%" looking-for)
+                 ;; (format t "looking-for-next: ~a~%" looking-for);
                  (solve looking-for (1- i))))))
-      (solve 2020 (1- instructions-length)))
+      ;;(format t "Initially: ~s~%" (solve 2020 (1- instructions-length)))
+      (solve 2020 (1- (* 100 instructions-length))
+             ;;(* 101741582076661 (1- instructions-length))
+             ;;(* 1 (1- instructions-length))
+             )
+      )
     ;; (iter
     ;;   (for cnt from 0 below 1)
     ;;   (with seen = (make-hash-table))
@@ -111,8 +146,24 @@
     ;; (aref cards 2020)
     ))
 
+;; Too low: 44082672264648
+;; 
+;; Got to tick: 527600000 before deciding that it wont make it before
+;; christmas(!)
+
+(defun parse-line-1 (line)
+  (let ((number (car (mapcar #'read-from-string (ppcre:all-matches-as-strings "[-0-9]+" line)))))
+    (cond
+      ((ppcre:all-matches-as-strings "cut" line)
+       (cons 'cut number))
+      ((ppcre:all-matches-as-strings "increment" line)
+       (cons 'inc number))
+      ((ppcre:all-matches-as-strings "stack" line)
+       (cons 'rev nil)))))
+
 (defun parse-line (line len)
   (let ((number (car (mapcar #'read-from-string (ppcre:all-matches-as-strings "[-0-9]+" line)))))
+    (declare (type fixnum len))
     (cond
       ((ppcre:all-matches-as-strings "cut" line)
        (lambda (idx) (let ((cut (if (< number 0)
@@ -120,22 +171,50 @@
                                     number)))
                        ;; (format t "Cutting: Cut: ~a, idx: ~a, len: ~a~%"
                        ;;         cut idx len)
+                       (declare (type fixnum idx))
+                       (declare (type fixnum cut))
+                       (declare (type fixnum number))
                        (mod (+ idx cut) len))))
       ((ppcre:all-matches-as-strings "increment" line)
-       (lambda (idx) (progn
+       (lambda (idx) (let ((idx-2 idx))
                        ;; (format t "Incing: len: ~a, num: ~a, mod: ~a~%"
                        ;;         len idx number)
-                       (solve-modulo len idx number))))
+                       (declare (type fixnum idx-2))
+                       (declare (type fixnum number))
+                       (solve-modulo len idx-2 number))))
       ((ppcre:all-matches-as-strings "stack" line)
-       (lambda (idx) (progn
+       (lambda (idx) (let ((idx-2 idx))
                        ;; (format t "Reving: rev: ~a, idx: ~a~%" idx len)
-                       (- (1- len) idx)))))))
+                       (declare (type fixnum idx-2))
+                       (- (1- len) idx-2)))))))
+
 
 (defun solve-modulo (len res grp)
+  (declare (type fixnum res))
   (iter
-    (for i from 0 below len)
-    (when (eq res (mod (* i grp) len))
-      (return i))))
+    (with pos = 0)
+    (declare (type fixnum pos))
+    (with i = 0)
+    (declare (type fixnum i))
+    (while (< pos len))
+    ;; Is there an even multiple of grp to get to res
+    (multiple-value-bind (d m) (floor (- res i) grp)
+      (declare (type fixnum d))
+      (declare (type fixnum m))
+     (when (eq 0 m)
+       (return (+ pos d))))
+    (multiple-value-bind (d) (floor (- len i) grp)
+      (declare (type fixnum d))
+      (incf pos (1+ d))
+      (incf i (* (1+ d) grp))
+      (setf i (mod i len))))
+  ;; (iter
+  ;;   (with i = 0)
+  ;;   (while (< i len))
+  ;;   (when (eq 0 (mod i res)))
+  ;;   (when (eq res (mod (* i grp) len))
+  ;;     (return i)))
+  )
 
 ;; Scratch area:
 
